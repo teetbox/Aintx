c5504d003ef9d2a3db743187ad108c56490905dc
ttionn <matt.tian@icloud.com>
2017-11-23 16:35:28 +0800

    Update JSONPlaceHolderTests

--- AintxTests/Integration/JSONPlaceHolderTests.swift
+++ AintxTests/Integration/JSONPlaceHolderTests.swift
@@ -11,10 +11,59 @@ import Aintx
 
 class JSONPlaceHolderTests: XCTestCase {
     
+    var aintx: Aintx!
+    var async: XCTestExpectation!
+    
+    struct Post: Codable {
+        let userId: Int
+        let id: Int
+        let title: String
+        let body: String
+        
+        enum CodingKeys: String, CodingKey {
+            case userId
+            case id
+            case title
+            case body
+        }
+    }
+    
     override func setUp() {
         super.setUp()
         
+        aintx = Aintx(base: "https://jsonplaceholder.typicode.com")
+        async = expectation(description: "async")
+    }
+    
+    func testSimpleGet() {
+        aintx.get("/posts/1") { response in
+            XCTAssertNotNil(response.data)
+            XCTAssertNil(response.error)
+            
+            let post = try! JSONDecoder().decode(Post.self, from: response.data!)
+            XCTAssertEqual(post.id, 1)
+            XCTAssertEqual(post.userId, 1)
+            XCTAssertEqual(post.title, "sunt aut facere repellat provident occaecati excepturi optio reprehenderit")
+            
+            self.async.fulfill()
+        }
         
+        wait(for: [async], timeout: 5)
+    }
+    
+    func testPost() {
+        let post = Post(userId: 88, id: 88, title: "TTST", body: "Forever")
+        let jsonData = try! JSONEncoder().encode(post)
+        let params = ["body": jsonData]
+        
+        aintx.post("/posts", params: params) { response in
+            XCTAssertNotNil(response.data)
+            XCTAssertNil(response.error)
+            
+            self.async.fulfill()
+        }
+        
+        wait(for: [async], timeout: 5)
     }
     
 }
