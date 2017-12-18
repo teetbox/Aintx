3830c0023626d677edc80c69bcf81eb7602d2e93
ttionn <matt.tian@icloud.com>
2017-12-18 21:49:54 +0800

    Update DataStructure

--- Aintx/Source/DataStructure.swift
+++ Aintx/Source/DataStructure.swift
@@ -29,26 +29,124 @@ public struct LinkedList<T> {
         return head == nil
     }
     
-    public mutating func add(_ element: T) {
-        let newNode = Node(value: element)
+    public mutating func append(_ element: T) {
+        let node = Node(value: element)
         
         if let tailNode = tail {
-            tailNode.next = newNode
-            newNode.previous = tailNode
+            tailNode.next = node
+            node.previous = tailNode
         } else {
-            head = newNode
+            head = node
         }
         
-        tail = newNode
+        tail = node
+    }
+    
+    public func node(at index: Int) -> Node<T>? {
+        guard index >= 0 else {
+            return nil
+        }
+        
+        var node = head
+        var i = 0
+        while node != nil {
+            if i == index {
+                return node
+            }
+            
+            node = node!.next
+            i += 1
+        }
+        
+        return nil
+    }
+    
+    @discardableResult
+    public mutating func remove(at index: Int) -> Node<T>? {
+        guard index >= 0 else {
+            return nil
+        }
+        
+        var node = head
+        var i = 0
+        while node != nil {
+            if i == index {
+                if let previousNode = node!.previous {
+                    if let nextNode = node!.next {
+                        previousNode.next = nextNode
+                        nextNode.previous = previousNode
+                    } else {
+                        previousNode.next = nil
+                        tail = previousNode
+                    }
+                } else {
+                    head = node!.next
+                    if head == nil {
+                        tail = nil
+                    }
+                }
+                return node
+            }
+            
+            node = node!.next
+            i += 1
+        }
+        
+        return nil
+    }
+    
+    @discardableResult
+    public mutating func remove(node: Node<T>) -> Node<T> {
+        let previous = node.previous
+        let next = node.next
+        
+        if let previous = previous {
+            previous.next = next
+        } else {
+            head = next
+        }
+        
+        next?.previous = previous
+        
+        if next == nil {
+            tail = previous
+        }
+        
+        node.previous = nil
+        node.next = nil
+        
+        return node
+    }
+    
+    public mutating func removeAll() {
+        head = nil
+        tail = nil
     }
     
 }
 
 public struct Queue<T> {
     
-    public func enqueue(_ element: T) {
-        
+    private var list = LinkedList<T>()
+    
+    public var isEmpty: Bool {
+        return list.isEmpty
     }
     
-
+    public mutating func enqueue(_ element: T) {
+        list.append(element)
+    }
+    
+    public mutating func dequeue() -> T? {
+        guard !isEmpty else {
+            return nil
+        }
+        
+        return list.remove(node: list.head!).value
+    }
+    
+    public func peek() -> T? {
+        return list.head?.value
+    }
+    
 }
--- AintxTests/Internal/DataStructureTests.swift
+++ AintxTests/Internal/DataStructureTests.swift
@@ -36,18 +36,82 @@ class DataStructureTests: XCTestCase {
         XCTAssertNil(list.head)
         XCTAssertNil(list.tail)
         
-        list.add(7)
+        list.append(7)
         XCTAssertNotNil(list.head)
         XCTAssertNotNil(list.tail)
         XCTAssertFalse(list.isEmpty)
+        
+        list.append(8)
+        let node = list.node(at: 1)
+        XCTAssertEqual(node?.value, 8)
+        
+        list.append(9)
+        var removedNode = list.remove(at: 1)
+        XCTAssertEqual(removedNode?.value, 8)
+        XCTAssertEqual(list.head?.value, 7)
+        XCTAssertEqual(list.tail?.value, 9)
+        XCTAssertEqual(list.head?.next?.value, 9)
+        
+        removedNode = list.remove(at: 1)
+        XCTAssertEqual(removedNode?.value, 9)
+        XCTAssertEqual(list.head?.value, 7)
+        XCTAssertEqual(list.tail?.value, 7)
+        
+        list.remove(at: 0)
+        XCTAssertNil(list.head)
+        XCTAssertNil(list.tail)
+        XCTAssert(list.isEmpty)
+        
+        list.append(1)
+        list.append(2)
+        list.append(3)
+
+        let node1 = list.node(at: 0)
+        let node2 = list.node(at: 1)
+        let node3 = list.node(at: 2)
+        XCTAssertEqual(node1?.value, 1)
+        XCTAssertEqual(node2?.value, 2)
+        XCTAssertEqual(node3?.value, 3)
+
+        let removedNode3 = list.remove(node: node3!)
+        XCTAssertEqual(removedNode3.value, 3)
+        XCTAssertEqual(list.head?.value, 1)
+        XCTAssertEqual(list.tail?.value, 2)
+
+        list.remove(node: node2!)
+        list.remove(node: node1!)
+        XCTAssertNil(list.head?.previous)
+        XCTAssertNil(list.tail?.next)
+        XCTAssert(list.isEmpty)
+        
+        list.append(4)
+        list.append(5)
+        list.append(6)
+        
+        list.removeAll()
+        XCTAssertNil(list.head?.previous)
+        XCTAssertNil(list.tail?.next)
+        XCTAssert(list.isEmpty)
     }
     
     func testQueue() {
-        let queue = Queue<Int>()
+        var queue = Queue<Int>()
+        XCTAssert(queue.isEmpty)
         
         queue.enqueue(5)
+        queue.enqueue(7)
+        XCTAssertFalse(queue.isEmpty)
         
+        var element = queue.dequeue()
+        XCTAssertEqual(element, 5)
         
+        element = queue.peek()
+        XCTAssertEqual(element, 7)
+        XCTAssertFalse(queue.isEmpty)
+        
+        element = queue.dequeue()
+        XCTAssertEqual(element, 7)
+        XCTAssert(queue.isEmpty)
     }
     
 }
