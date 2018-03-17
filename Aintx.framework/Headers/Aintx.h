e86bc6e861385a9dbd00df6c98dfb21563c2f385
ttionn <matt.tian@icloud.com>
2018-03-17 20:08:41 +0800

    Add universal build target

---
+++ Aintx.framework/Headers/Aintx.h
@@ -0,0 +1,19 @@
+//
+//  Aintx.h
+//  Aintx
+//
+//  Created by Tong Tian on 21/10/2017.
+//  Copyright © 2017 Bizersoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Aintx.
+FOUNDATION_EXPORT double AintxVersionNumber;
+
+//! Project version string for Aintx.
+FOUNDATION_EXPORT const unsigned char AintxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Aintx/PublicHeader.h>
+
+
